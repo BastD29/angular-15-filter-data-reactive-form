82e37e6744b2582633fb62a9ba21ef52d3b054b7
Bastien Daniel <bastien.daniel@rawfish.com>
2023-06-23 14:10:49 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
+interface Item {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-select-option-rerender';
+  form!: FormGroup;
+  items!: Item[];
+  data: {
+    optionId: number;
+    title: string;
+  }[] = [
+    {
+      title: 'Data 1',
+      optionId: 1,
+    },
+    {
+      title: 'Data 2',
+      optionId: 1,
+    },
+    {
+      title: 'Data 3',
+      optionId: 2,
+    },
+    {
+      title: 'Data 4',
+      optionId: 3,
+    },
+    {
+      title: 'Data 5',
+      optionId: 2,
+    },
+  ];
+
+  public filteredData: any[] = this.data;
+
+  constructor(private fb: FormBuilder) {
+    this.items = [
+      { id: 1, name: 'Option 1' },
+      { id: 2, name: 'Option 2' },
+      { id: 3, name: 'Option 3' },
+    ];
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      selectedItem: [''],
+    });
+
+    this.form.get('selectedItem')?.valueChanges.subscribe((value) => {
+      this.filteredData = this.data.filter((item) => {
+        console.log('--value--', value);
+        console.log('--item--', item);
+        return value ? item.optionId === Number(value) : item;
+      });
+
+      console.log(this.filteredData);
+    });
+  }
 }
